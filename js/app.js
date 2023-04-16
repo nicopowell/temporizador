00e22f7814d1593c50cd4040a8560f96facfe9f7
Nicolas Powell <nicopowell30@gmail.com>
2023-04-16 12:37:17 -0300

    armé la estructura basica al proyecto

---
+++ js/app.js
@@ -0,0 +1,36 @@
+section {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: #0009;
+  width: 90%;
+  max-width: 400px;
+  min-height: 220px;
+  border: 2px solid #fff9;
+}
+
+section p {
+  font-size: 3.5rem;
+  margin: 0;
+}
+
+.btn {
+  padding: 0 0.5rem;
+}
+
+.btn {
+  padding: 0 0.5rem;
+}
+
+i {
+  font-size: 4rem;
+  color: #ddd;
+}
+
+i:hover {
+  color: #ddddddc2;
+}
+
+i:active {
+  color: #dddddd77;
+}
